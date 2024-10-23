4bd103a1d05bd1f4bfad024cdd9d2b8020b2314c
Chris Friedberg <chris.friedberg@alaskaair.com>
2024-10-23 09:35:22 -0700

    Basic Hello World Story

--- src/components/HelloWorld/HelloWorld.stories.tsx
+++ src/components/HelloWorld/HelloWorld.stories.tsx
@@ -1,4 +1,3 @@
-import { html } from 'lit';
 import type { Meta, StoryObj } from '@storybook/web-components';
 import './HelloWorld';
 
@@ -10,35 +9,12 @@ const meta = {
   title: 'Components/HelloWorld',
   tags: ['autodocs'],
   component: 'auro-hello-world',
-  parameters: {
-    docs: {
-      description: {
-        component: 'A simple Auro hello world component that displays a greeting with a customizable name.',
-      },
-    },
-  },
-  render: (args) => html`
-    <auro-hello-world 
-      .name=${args.name}
-    ></auro-hello-world>
-  `,
-  argTypes: {
-    name: {
-      control: 'text',
-      description: 'Name to display in the greeting',
-      defaultValue: 'Alaska Air',
-    }
-  },
 } satisfies Meta<HelloWorldProps>;
 
 export default meta;
 type Story = StoryObj<HelloWorldProps>;
 
-export const Default: Story = {
-  args: {
-    name: 'Somebody',
-  },
-};
+export const Default: Story = {};
 
 export const CustomName: Story = {
   args: {
